79dc92ddc2d33bb230d0322cbe790892acefa4bc
valkov-artyom <valkov.artyom@gmail.com>
2020-08-13 06:31:30 +0300

    add ssr

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import {MainLayout} from "../layouts/Layout";
+import classes from '../styles/error.module.scss'
+
+export default function ErrorPage() {
+    return (
+        <MainLayout>
+            <h1 className={classes.error}>Error 404</h1>
+            <p>Please <Link href={'/'}><a> go back to safety</a></Link></p>
+        </MainLayout>
+    )
+}
--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,10 +1,46 @@
+import {useState, useEffect} from 'react'
+import {MainLayout} from "../../layouts/Layout";
+import Link from 'next/link'
 import {useRouter} from "next/router";
-import {route} from "next/dist/next-server/server/router";
 
-export default function Post() {
-    const router =  useRouter()
-    console.log(router)
-    return <>
-        <h1>Post {router.query.id}</h1>
-    </>
+export default function Post({ post: serverPost }) {
+    const [post, setPost] = useState(serverPost)
+    const router = useRouter();
+
+    useEffect(() => {
+        async function load() {
+            const response  = await fetch(`http://localhost:4200/posts/${router.query.id}`);
+            const data = await response.json();
+            setPost(data)
+        }
+        if (!post) {
+            load()
+        }
+    }, [])
+
+    if (!post) {
+        return <MainLayout>
+            <p>Loading...</p>
+        </MainLayout>
+    }
+
+    return <MainLayout>
+        <h1>{post.title}</h1>
+        <hr/>
+        <p>{post.body}</p>
+        <Link href={`/posts`}><button>Go to posts page</button></Link>
+    </MainLayout>
 }
+
+export async function getServerSideProps ({query, req}){
+    if (!req) {
+        return {post: null}
+    }
+    const response  = await fetch(`http://localhost:4200/posts/${query.id}`);
+    const post = await response.json();
+
+    return {
+        props : {post}
+    }
+}
+
